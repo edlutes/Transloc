9a8862671668c3bbde6fe46b7617f375aba85967
Ed Lutes <ed_lutes@yahoo.com>
2019-05-23 11:54:10 -0400

    CSV to json working with only lat/long remaining

--- CSVconverter.py
+++ CSVconverter.py
@@ -1,25 +1,35 @@
 import csv
 import json
+import pandas as pd
+import os 
 
-file = '../GeoLite2-City-CSV_20190521/GeoLite2-City-Blocks-IPv4-smallest-test.csv'
+# For faster testing use a smaller subset
+#file = '../GeoLite2-City-CSV_20190521/GeoLite2-City-Blocks-IPv4-smallest-test.csv'
+file = '../GeoLite2-City-CSV_20190521/GeoLite2-City-Blocks-IPv4.csv'
 json_file = './parsedCSV.json'
-# Only care about Lat/Long for this
+lat_long_only_file = './latlong.csv'
+# Only care about Lat/Long for this, if that changes need to remove from unwanted_keys
 present_headers = ['network','geoname_id','registered_geoname_id','represented_country_geoname_id','is_anonymous_proxy','is_satellite_provider','postal_code','latitude','longitude','accuracy_radius']
-headers = ['longitude', 'latitude']
 unwanted_keys=['network','geoname_id','registered_geoname_id','represented_country_geoname_id','is_anonymous_proxy','is_satellite_provider','postal_code','accuracy_radius']
 
+# Use Panda to pre-process CSV file and remove all columns except latitude and longitude
+# Can use the existance of the latlong.csv file as a trigger to update the main data
+df1 = pd.read_csv(file, names = present_headers, header = 0)
+df2 = df1.drop(unwanted_keys, axis =1)
+df3 = df2.to_csv(lat_long_only_file, index = False)
 
-
+# Convert latlong.csv into parsedCSV.json
 csv_rows = []
-with open(file) as rf:
+with open(lat_long_only_file) as rf:
     reader = csv.DictReader(rf)
     title = reader.fieldnames
     for row in reader:
-        for i in range(len(title)):
-            if title[i] in headers:
-               csv_rows.extend([{title[i]:row[title[i]] for i in range(len(title))}])
+        csv_rows.extend([{title[i]:row[title[i]] for i in range(len(title))}])
     with open(json_file, 'w') as wf:    
         wf.write(json.dumps(csv_rows, sort_keys=False, indent=4, separators=(',', ': '),encoding="utf-8",ensure_ascii=False))
+# remove latlong.csv file
+os.remove(lat_long_only_file)
+
 
 
         
