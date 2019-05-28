4eb0eb3300504feb72067b5cd3be1871d1f0faf4
Ed Lutes <ed_lutes@yahoo.com>
2019-05-28 07:50:54 -0400

    comment out AWS attempt

--- static/js/my_map.js
+++ static/js/my_map.js
@@ -15,6 +15,7 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: 'pk.eyJ1IjoiZWx1dGVzIiwiYSI6ImNqdnlnMWYwazAycG40YmxjYzB0bzlhZzEifQ.DXfl1jtvxEfFrmqPdn7D6Q'
 }).addTo(map);
 
+
 // Attempt at pulling from AWS
 // console.log("wut?")
 // function pull_aws() {
@@ -42,37 +43,52 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 //     })();
 // };
 
-var AWS = require('aws-sdk');
-AWS.config.update(
-  {
-    // These will be deleted shortly
-    accessKeyId: "AKIAJESJ6AUWDUBA67JQ",
-    secretAccessKey: "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",
-    region: 'us-east-1'
-  }
-);
-var s3 = new AWS.S3();
-s3.getObject(
-  { Bucket: "lutestransloc", Key: "GeoObs.geojson" },
-  function (error, data) {
-    if (error != null) {
-      alert("Failed to retrieve an object: " + error);
-    } else {
-      alert("Loaded " + data.ContentLength + " bytes");
-      // Not sure if this works and takes forever or is completely wrong
-      console.log(huh);
-      $.getJSON("../files/GeoObs.geojson",function(data){
-        var locations = data.features.map(function(rat){
-            var location = rat.geometry.coordinates;
-            location.push(0.5);
-            return location;
-        });
+
+// 2nd attempt at AWS
+
+// var AWS = require('aws-sdk');
+// AWS.config.update(
+//   {
+//     // These will be deleted shortly
+//     accessKeyId: "AKIAJESJ6AUWDUBA67JQ",
+//     secretAccessKey: "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",
+//     region: 'us-east-1'
+//   }
+// );
+// var s3 = new AWS.S3();
+// s3.getObject(
+//   { Bucket: "lutestransloc", Key: "GeoObs.geojson" },
+//   function (error, data) {
+//     if (error != null) {
+//       alert("Failed to retrieve an object: " + error);
+//     } else {
+//       alert("Loaded " + data.ContentLength + " bytes");
+//       // Not sure if this works and takes forever or is completely wrong
+//       console.log(huh);
+//       $.getJSON("../files/GeoObs.geojson",function(data){
+//         var locations = data.features.map(function(rat){
+//             var location = rat.geometry.coordinates;
+//             location.push(0.5);
+//             return location;
+//         });
     
-        var heat = L.heatLayer(locations, {radius: 35}).addTo(map);
-        heat.addTo(map)
+//         var heat = L.heatLayer(locations, {radius: 35}).addTo(map);
+//         heat.addTo(map)
     
+//     });
+//     }
+//   }
+// );
+
+$.getJSON("../files/GeoObs.geojson",function(data){
+    var locations = data.features.map(function(rat){
+        var location = rat.geometry.coordinates;
+        location.push(0.5);
+        return location;
     });
-    }
-  }
-);
+
+    var heat = L.heatLayer(locations, {radius: 35}).addTo(map);
+    heat.addTo(map)
+
+});
 
